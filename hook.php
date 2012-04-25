504e811a61726d0c1433bd3ad70992db851297e0
Louis Bodnar <pezmanlou@gmail.com>
2012-04-25 16:42:47 -0400

    lolol

--- hook.php
+++ hook.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /srv/www/Spirographics/ && touch penis' );
+  shell_exec( 'cd /srv/www/Spirographics/ && touch penis' ); // lol
   shell_exec( 'cd /srv/www/Spirographics/ && git reset --hard HEAD && git pull' );
 }
 
