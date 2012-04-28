d2b967d3c72c3bd35e2fa60dd2f1d108c7d75a11
louis <louis@minibeast.(none)>
2012-04-27 21:04:08 -0400

    update hook

--- hook.php
+++ hook.php
@@ -4,7 +4,7 @@
 
 //if ( $_POST['payload'] ) {
   shell_exec( 'touch /log/hook-success' );
-  $output = shell_exec( 'sudo -u www-data git pull' );
+  $output = shell_exec( 'git pull' );
   //$output = shell_exec( 'pwd' );
   echo "$output";
 //}
