542c12fb971b19e7758de91af90a98fbb9f9ee85
Mike <mike@what.(none)>
2012-04-28 16:12:39 -0400

    fuck

--- update.php
+++ update.php
@@ -16,11 +16,12 @@ $sql = "SELECT * FROM 'graphs' WHERE 1";
 $result = mysql_query($sql);
 
 
-$num = mysql_numrows($result);
 
-echo $num
 
 mysql_free_result($result);
 
 
 ?>
+
+
+a;sldjflaksdjfasdl;ajf
