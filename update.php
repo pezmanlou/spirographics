43bb75a7654b16589b55ec1775f9963bbeea4672
Mike <mike@what.(none)>
2012-04-28 15:58:32 -0400

    fuck

--- update.php
+++ update.php
@@ -6,7 +6,7 @@ require("connect.php");
 
 //echo $_POST["fixed"];
 
-mysql_query("INSERT into graphs (fixed, moving,offset) VALUES ( '$_POST[fixed]', '$_POST[moving]', '$_POST[offset]')";
+//mysql_query("INSERT into graphs (fixed, moving,offset) VALUES ( '$_POST[fixed]', '$_POST[moving]', '$_POST[offset]')";
 
 //echo $_SERVER['REMOTE_ADDR'];  // echos ip address of client
 ?>
