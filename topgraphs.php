6702c95c9b2cde1983f4dc27735260d12eb319a6
Mike <mike@what.(none)>
2012-04-28 18:52:01 -0400

    fuck

---
+++ topgraphs.php
@@ -0,0 +1,13 @@
+<?php
+require("connect.php");
+
+$result = mysql_query("SELECT * FROM graphrank ORDER BY rank ASC LIMIT 0,10");
+
+while($row = mysql_fetch_array($result))
+{
+	echo $row['rank'].'<br />';
+
+
+}
+
+?>
