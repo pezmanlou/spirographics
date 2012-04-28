822ac7995901cd0836e91186c0c19536548d112d
Mike <mike@what.(none)>
2012-04-28 17:08:46 -0400

    fuck

--- update.php
+++ update.php
@@ -17,7 +17,7 @@ $sql = "SELECT * FROM graphs WHERE fixed='$fixed' AND moving='$moving' AND offse
 $result = mysql_query($sql);
 if(mysql_num_rows($result)==0)
 {
-	$insert = "INSERT INTO graphs VALUES (", '$fixed', '$moving', '$offset',1,0,'$clientIp'")"; 
+	$insert = "INSERT INTO graphs VALUES (", '$fixed', '$moving', '$offset','1','0','$clientIp'")"; 
 	$newChart = mysql_query($insert);
 }
 else
