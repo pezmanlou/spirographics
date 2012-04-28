f7c8534c43c58481c894d3d34fbcf4da32692ad0
Mike <mike@what.(none)>
2012-04-28 17:34:00 -0400

    fuck

--- update.php
+++ update.php
@@ -24,7 +24,7 @@ if(mysql_num_rows($result)==0)
 }
 else
 {
-	echo 'here'
+	echo 'here';
 	$update = "UPDATE graphs counter=counter+1 WHERE fixed='$fixed' AND moving='$moving' AND offset='$offset'";
 	$updateChart= mysql_query($update);
 	
