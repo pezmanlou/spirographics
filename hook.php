f9c4274633b6e3cd60ca8f0496ba8e1dc88f1dc0
louis <louis@minibeast.(none)>
2012-04-27 20:04:21 -0400

    update hook

--- hook.php
+++ hook.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /log && touch penis' ); // oll
+  shell_exec( 'cd /log && touch penis' ); // ol
   shell_exec( 'cd /var/www/Spirographics/ && git reset --hard HEAD && git pull' > );
 }
 
