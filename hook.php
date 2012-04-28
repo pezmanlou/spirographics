9286d5b7e8a6625882cbec0881c572f9386cfe1a
louis <louis@minibeast.(none)>
2012-04-27 20:53:14 -0400

    update hook

--- hook.php
+++ hook.php
@@ -1,12 +1,12 @@
-d<?php
+-<?php
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 //if ( $_POST['payload'] ) {
   shell_exec( 'touch /log/hook-success' );
-  $output = shell_exec( 'whoami' );
+  $output = shell_exec( 'git pull' );
   //$output = shell_exec( 'pwd' );
   echo "$output";
 //}
 
-?>b
+?>-
