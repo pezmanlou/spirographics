5853af5c4a214f18b35e07194b3bc90d719bc15b
Mike <mike@what.(none)>
2012-04-28 16:00:36 -0400

    fuck

--- update.php
+++ update.php
@@ -9,4 +9,20 @@ require("connect.php");
 //mysql_query("INSERT into graphs (fixed, moving,offset) VALUES ( '$_POST[fixed]', '$_POST[moving]', '$_POST[offset]')";
 
 //echo $_SERVER['REMOTE_ADDR'];  // echos ip address of client
+
+$dbname = "spiro";
+
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_row($result)) {
+   
+
+
 ?>
