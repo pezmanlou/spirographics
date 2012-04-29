7be8ca5055f02e71ccb00f6a4ce6bfa0c5e55c35
Mike <mike@what.(none)>
2012-04-28 21:11:55 -0400

    trying

--- index.php
+++ index.php
@@ -232,8 +232,11 @@ function getLink()
 <h3>Do you like this graph?</h3></p></div>
 <div class="well sidebar-nav" id="sidebar" >
             <div style="width:48%; float:left"><input type ="submit" value="Download" onclick="save()" class="btn btn-primary btn-medium" /></div>
-	       <div style="width:48%; float:right" id ="link"><input type ="submit" value="Short Url" onclick="getLink()" class="btn btn-primary btn-medium" /></div>
+	       <div style="width:48%; float:right"><input type ="submit" value="Short Url" onclick="getLink()" class="btn btn-primary btn-medium" /></div>
+<div id="test">
 
+
+</div>	
 </div>
         </div><!--/span-->
  <div class="span9" id="spiroWidth">
