83c416f38c99f88261f43a5e3449bcdc5d3a76a0
Mike <mike@what.(none)>
2012-04-28 23:30:05 -0400

    hopefully

--- index.php
+++ index.php
@@ -170,7 +170,7 @@ function newAlert (type, message) {
 }*/
 
 function newAlert (type, message) {
-    $("#alert-area").append($("<div class='alert alert-success'> <a class='close' data-dismiss='alert'>×</a>" + type + " </div>"));
+    $("#alert-area").append($("<div class='alert alert-"+type+"> <a class='close' data-dismiss='alert'>×</a>" + type + " </div>"));
 }
 
 
