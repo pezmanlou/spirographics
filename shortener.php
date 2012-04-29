779f1a80652453dba604970dca192ddb86603feb
Mike <mike@what.(none)>
2012-04-28 21:08:20 -0400

    trying

--- shortener.php
+++ shortener.php
@@ -1,7 +1,32 @@
-<?php
+<?php 
+
+//Get Post Variables. The name is the same as 
+//what was in the object that was sent in the jQuery
+if (isset($_POST['sendValue'])){
+    $value = $_POST['sendValue'];   
+}else{
+    $value = "";
+}
+
+//Because we want to use json, we have to place things in an array and encode it for json.
+//This will give us a nice javascript object on the front side.
+echo json_encode(array("returnValue"=>"This is returned from PHP : ".$value));  
+
+?>
 
 
 
+
+
+
+
+
+
+
+
+
+
+/*
 $rest = "http://fatlink.me/api.php?action=shorturl&format=simple&keyword=test&url=http://triallink.com";
 $response = file_get_contents($rest);
 echo $response;
