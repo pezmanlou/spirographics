1066cf28dfb50693b5931efbb0516798a7b9267b
Mike <mike@what.(none)>
2012-04-28 21:21:23 -0400

    trying

--- shortener.php
+++ shortener.php
@@ -2,6 +2,8 @@
 
 //Get Post Variables. The name is the same as 
 //what was in the object that was sent in the jQuery
+$response = "test";
+
 if (isset($_POST['sendValue'])){
     $rest = $_POST['sendValue']; 
     $response = file_get_contents($rest);
