eb4ed0d365f01497f7df8ccfaed6e1279e03a0ad
Mike <mike@what.(none)>
2012-04-28 21:17:54 -0400

    trying

--- shortener.php
+++ shortener.php
@@ -7,29 +7,5 @@ if (isset($_POST['sendValue'])){
 }else{
     $value = "";
 }
-
-//Because we want to use json, we have to place things in an array and encode it for json.
-//This will give us a nice javascript object on the front side.
-echo json_encode(array("returnValue"=>"This is returned from PHP : ".$value));  
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$rest = "http://fatlink.me/api.php?action=shorturl&format=simple&keyword=test&url=http://triallink.com";
-$response = file_get_contents($rest);
-echo $response;
-
-
+ return $value;
 ?>
