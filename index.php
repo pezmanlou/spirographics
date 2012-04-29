6000dfb2d182fb6763ed87e5713c7556c73c4df6
Mike <mike@what.(none)>
2012-04-29 19:32:32 -0400

    hey

--- index.php
+++ index.php
@@ -128,8 +128,7 @@ $('input[name=fixed]').val(fixed);
 $('input[name=vote]').val("up");
  $.post("update.php",{ fixed:fixed, moving:moving, offset:offset, vote:"up" },
             function(data){
-		alert("-"+data+"-");
-		if(data =="no")
+		if(data =="no\n")
 		{
 			newAlert('error', 'You have Already Voted on this Graph');
 		}
