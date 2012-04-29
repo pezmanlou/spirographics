6f5df8934199b80eed546e0d24b757db0ebcbca6
Mike <mike@what.(none)>
2012-04-28 21:45:31 -0400

    trying

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ $('input[name=vote]').val("down");
 //document.bonerjams08.submit();
  $.post("update.php",{ fixed: fixed, moving:moving, offset:offset, vote:"down"},
             function(data){
-               alert(data);
+               $(".alert").alert(data);
             });
 
 
