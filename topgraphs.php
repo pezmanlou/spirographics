a15372ca98ea64d0ee19ab738b80e5b22621d2f1
Mike <mike@what.(none)>
2012-04-29 21:49:53 -0400

    shit

--- topgraphs.php
+++ topgraphs.php
@@ -122,14 +122,7 @@ function thumbnail(c, f, m, o){
 	?>	      
 	</div>
 
-    <form class="well form-inline">
-    <input type="text" class="input-small" placeholder="Email">
-    <input type="password" class="input-small" placeholder="Password">
-    <label class="checkbox">
-    <input type="checkbox"> Remember me
-    </label>
-    <button type="submit" class="btn">Sign in</button>
-    </form>
+
    </div>
       <footer>
         <p>&copy; tehInternetzd00d (we are ironic) 2012</p>
