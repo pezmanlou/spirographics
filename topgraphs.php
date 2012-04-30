9a0af982a0ba7b7c8c54031e1997a5892732688f
louis <louis@minibeast.(none)>
2012-04-30 18:08:13 -0400

    working on scale

--- topgraphs.php
+++ topgraphs.php
@@ -17,14 +17,14 @@ function thumbnail(c, f, m, o){
     var y = 0;
 
 
-  alert("here");
+
     // move to starting point
     x = (f - m)*Math.cos(0) + o*Math.cos(0);
     y = (f - m)*Math.sin(0) - o*Math.sin(0);
     context.moveTo(canvas.width/2 + x,canvas.height/2 + y);
     context.beginPath();
     var maxtheta = (Math.abs(m)/GCD(Math.abs(f),Math.abs(m)))*2*Math.PI;
-
+  alert("here");
     // then draw the line
     for (var tempTheta = 0; tempTheta <= maxtheta; tempTheta = tempTheta + .01)
     {
