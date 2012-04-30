37a2571ab9a19995a553d824ae9fc3bb4de3e203
Mike <mike@what.(none)>
2012-04-29 21:56:26 -0400

    shit

--- topgraphs.php
+++ topgraphs.php
@@ -116,6 +116,7 @@ function thumbnail(c, f, m, o){
 				<script>thumbnail(".$row['penis'].",".$row['fixed'].",".$row['moving'].",".$row['offset']." )</script>
 
 			</div>
+			test
 			</div>";	
 
 }
