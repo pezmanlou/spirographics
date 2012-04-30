c274214866fde0ffe1517cb13a004b02b7aed349
Mike <mike@what.(none)>
2012-04-29 22:08:36 -0400

    shit

--- topgraphs.php
+++ topgraphs.php
@@ -110,7 +110,7 @@ function thumbnail(c, f, m, o){
 	<?php $result = mysql_query("SELECT * FROM graphrank ORDER BY rank DESC LIMIT 0,10");
 	while($row = mysql_fetch_array($result))
 {
-	echo "<div class='span6'>
+	echo "<div class='span6' style='background-color:#9090;'>
 			<div class='well'>
 				 <canvas id='".$row['penis']."' style='height:150px; width:300px;' class='well' />
 				<script>thumbnail(".$row['penis'].",".$row['fixed'].",".$row['moving'].",".$row['offset']." )</script>
