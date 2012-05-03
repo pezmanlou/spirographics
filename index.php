12ea918206652c49bbe11b7aed022a9cf4121319
louis <louis@minibeast.(none)>
2012-05-02 22:02:33 -0400

    poopshitdick

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>SpiroGraphics for the kids??</title>
+    <title>SpiroGraphics for the kids?</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
